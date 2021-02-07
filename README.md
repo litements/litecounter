91f57e621af0654102c662e1791661b0845b5e99
rickaa <37962604+d0l-phin@users.noreply.github.com>
2021-02-07 18:08:17 +0100

    init

---
+++ README.md
@@ -0,0 +1,107 @@
+# litecounter
+
+> Very simple counter implemented on top of SQLite
+
+## Why?
+
+You can use this to implement a persistent counter. It also uses some SQLite syntax to initialize keys to `0` when the counter starts on them., just as if you had a `collections.defaultdict` where the default is `0`.
+
+## Installation
+
+## Examples
+
+The examples are taken from the tests in [`tests.ipynb`](./tests.ipynb)
+
+
+```python
+
+TEST_1 = "key_test_1"
+TEST_2 = "key_test_2"
+
+from litecounter import SQLCounter
+
+counter = SQLCounter(":memory:")
+
+# Increment from 0 to 20
+
+for _ in range(20):
+    counter.incr(TEST_1)
+    
+assert counter.count(TEST_1) == 20
+
+# Decrement 10 (from 20 to 10)
+
+for _ in range(10):
+    counter.decr(TEST_1)
+    
+assert counter.count(TEST_1) == 10
+
+# From 0 to -10, then -20.
+
+for _ in range(10):
+    counter.decr(TEST_2)
+    
+assert counter.count(TEST_2) == -10
+
+for _ in range(10):
+    counter.decr(TEST_2)
+    
+assert counter.count(TEST_2) == -20
+
+# Set fist key to 0.
+
+counter.zero(TEST_1)
+
+assert counter.count(TEST_1) == 0
+
+# Increment the second test key by 100, from -20 to 80.
+
+for _ in range(100):
+    counter.incr(TEST_2)
+    
+assert counter.count(TEST_2) == 80
+
+# Delete key works
+
+assert counter.count(TEST_1) == 0
+
+counter.delete(TEST_1)
+
+assert counter.count(TEST_1) is None
+
+# When the key does not exist, delete just ignores it
+
+counter.delete("foobar")
+
+# Check `__repr__`
+
+import random
+
+for key in ["foo", "bar", "baz", "foobar", "asd", TEST_1]:
+    for _ in range(random.randint(0,10)):
+        counter.incr(key)
+
+print(counter)
+
+# SQLCounter(dbname=':memory:', items=[('key_test_2', 80), ('foo', 8), ('baz', 5), ('foobar', 6), ('key_test_1', 10)])
+```
+
+    
+## Meta
+
+
+Ricardo Ander-Egg Aguilar – [@ricardoanderegg](https://twitter.com/ricardoanderegg) –
+
+- [ricardoanderegg.com](http://ricardoanderegg.com/)
+- [github.com/polyrand](https://github.com/polyrand/)
+- [linkedin.com/in/ricardoanderegg](http://linkedin.com/in/ricardoanderegg)
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
+
+## Contributing
+
+The only hard rules for the project are:
+
+* No extra dependencies allowed
+* No extra files, everything must be inside the main module's `.py` file.
+* Tests must be inside the `tests.ipynb` notebook.
---
+++ litecounter.py
@@ -0,0 +1,94 @@
+import sqlite3
+from contextlib import contextmanager
+import pprint
+
+__version__ = "0.1"
+
+# SQLite works better in autocommit mode when using short DML (INSERT / UPDATE / DELETE) statements
+# source: https://charlesleifer.com/blog/going-fast-with-sqlite-and-python/
+@contextmanager
+def transaction(conn: sqlite3.Connection):
+    # We must issue a "BEGIN" explicitly when running in auto-commit mode.
+    conn.execute("BEGIN")
+    try:
+        # Yield control back to the caller.
+        yield conn
+    except:
+        conn.rollback()  # Roll back all changes if an exception occurs.
+        raise
+    else:
+        conn.commit()
+
+
+class SQLCounter:
+    def __init__(
+        self,
+        dbname,
+        check_same_thread=False,
+        fast=True,
+        **kwargs,
+    ):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(
+            self.dbname,
+            check_same_thread=check_same_thread,
+            isolation_level=None,
+            **kwargs,
+        )
+
+        with transaction(self.conn) as c:
+
+            c.execute(
+                "CREATE TABLE IF NOT EXISTS Counter (key text NOT NULL PRIMARY KEY, value integer)"
+            )
+
+        if fast:
+            self.conn.execute("PRAGMA journal_mode = 'WAL';")
+            self.conn.execute("PRAGMA temp_store = 2;")
+            self.conn.execute("PRAGMA synchronous = 1;")
+            self.conn.execute(f"PRAGMA cache_size = {-1 * 64_000};")
+
+    def incr(self, key):
+        self.conn.execute(
+            "INSERT INTO Counter VALUES (?, 1) ON CONFLICT(key) DO UPDATE SET value = value + 1",
+            (key,),
+        )
+        return
+
+    def decr(self, key):
+        self.conn.execute(
+            "INSERT INTO Counter VALUES (?, -1) ON CONFLICT(key) DO UPDATE SET value = value - 1",
+            (key,),
+        )
+        return
+
+    def count(self, key):
+        c = self.conn.execute("SELECT value FROM Counter WHERE Key=?", (key,))
+        row = c.fetchone()
+        if row is None:
+            return None
+        return row[0]
+
+    def zero(self, key):
+        self.conn.execute(
+            "INSERT INTO Counter VALUES (?, 0) ON CONFLICT(key) DO UPDATE SET value = 0",
+            (key,),
+        )
+        return
+
+    def delete(self, key):
+        """
+        Delete counter key, if the key does NOT exist
+        it will just ignore it (no exceptions raised)
+        """
+
+        self.conn.execute("DELETE FROM Counter WHERE key=?", (key,))
+
+    def __repr__(self):
+        return f"{type(self).__name__}(dbname={self.dbname!r}, items={pprint.pformat(self.conn.execute('SELECT * FROM Counter').fetchall())})"
+
+    def vacuum(self):
+        self.conn.execute("VACUUM;")
+
+    def close(self):
+        self.conn.close()
