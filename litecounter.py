eda9c1450ed56963bf026fc66bc7b9e8126baaa2
polyrand <37962604+d0l-phin@users.noreply.github.com>
2021-08-07 13:27:08 +0200

    make linter happy

--- litecounter.py
+++ litecounter.py
@@ -5,6 +5,7 @@ import pprint
 
 __version__ = "0.2"
 
+
 # SQLite works better in autocommit mode when using short DML (INSERT / UPDATE / DELETE) statements
 # source: https://charlesleifer.com/blog/going-fast-with-sqlite-and-python/
 @contextmanager
@@ -14,27 +15,34 @@ def transaction(conn: sqlite3.Connection):
     try:
         # Yield control back to the caller.
         yield conn
-    except:
+    except BaseException as e:
         conn.rollback()  # Roll back all changes if an exception occurs.
-        raise
+        raise e
     else:
         conn.commit()
 
 
 class SQLCounter:
     def __init__(
-        self, filename_or_conn=None, memory=False, **kwargs,
+        self,
+        filename_or_conn=None,
+        memory=False,
+        **kwargs,
     ):
         assert (filename_or_conn is not None and not memory) or (
             filename_or_conn is None and memory
         ), "Either specify a filename_or_conn or pass memory=True"
+
         if memory or filename_or_conn == ":memory:":
             self.conn = sqlite3.connect(":memory:", isolation_level=None, **kwargs)
+
         elif isinstance(filename_or_conn, (str, pathlib.Path)):
             self.conn = sqlite3.connect(
                 str(filename_or_conn), isolation_level=None, **kwargs
             )
+
         else:
+            assert filename_or_conn is not None
             self.conn = filename_or_conn
             self.conn.isolation_level = None
 
